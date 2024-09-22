6cf65581b41e035096d44024a2218249881f574a
aniket30i <iamaniket.saha.09@protonmail.com>
2024-09-22 19:54:28 +0530

    fonts 2

--- pft-str/starter-template/src/app/layout.tsx
+++ pft-str/starter-template/src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, Calistoga } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: "400",
+});
 export const metadata: Metadata = {
   title: "My Portfolio",
   description: "Created with the help of Frontend Tribe",
