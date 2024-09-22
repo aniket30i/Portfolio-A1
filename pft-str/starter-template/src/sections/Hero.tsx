d5dfe0aed3e1cc6d52e60ccb5becaf8aee211955
aniket30i <iamaniket.saha.09@protonmail.com>
2024-09-22 23:58:11 +0530

    Work on hero

--- pft-str/starter-template/src/sections/Hero.tsx
+++ pft-str/starter-template/src/sections/Hero.tsx
@@ -1,3 +1,16 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <Image src={memojiImage} alt="Person behind laptop" />
+        <div>
+          <div></div>
+          <div>Available for new Projects</div>
+        </div>
+      </div>
+    </div>
+  );
 };
