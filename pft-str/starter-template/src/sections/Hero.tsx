cc7766c73f79ce895862d454e4c972d908e9807f
aniket30i <iamaniket.saha.09@protonmail.com>
2024-09-23 00:05:19 +0530

    hero settings

--- pft-str/starter-template/src/sections/Hero.tsx
+++ pft-str/starter-template/src/sections/Hero.tsx
@@ -1,14 +1,37 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 
 export const HeroSection = () => {
   return (
-    <div>
+    <div className="py-32">
       <div className="container">
-        <Image src={memojiImage} alt="Person behind laptop" />
         <div>
-          <div></div>
-          <div>Available for new Projects</div>
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person behind laptop"
+          />
+          <div>
+            <div></div>
+            <div>Available for new Projects</div>
+          </div>
+        </div>
+
+        <h1>Building Expectional User experiences</h1>
+        <p>
+          I specialize in transforming designs into functional, appealing web
+          applications. Lets discuss about a project and team up
+        </p>
+        <div>
+          <button>
+            <span>Explore my Work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span>🖐</span>
+            <span>Let's Connect</span>
+          </button>
         </div>
       </div>
     </div>
