b0762a559ad02ba289a2934cac02e96257a86bd2
aniket30i <iamaniket.saha.09@protonmail.com>
2024-09-23 20:10:35 +0530

    about section designs

---
+++ pft-str/starter-template/src/components/TechIcon.tsx
@@ -0,0 +1,6 @@
+const TechIcon = ({ component }: { component: React.ElementType }) => {
+  const Component = component;
+  return <Component className="size-10" />;
+};
+
+export default TechIcon;
--- pft-str/starter-template/src/sections/About.tsx
+++ pft-str/starter-template/src/sections/About.tsx
@@ -1,3 +1,119 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import TechIcon from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import { title } from "process";
+import CardHeader from "@/components/CardHeader";
+import ToolBoxItem from "@/components/ToolBoxItem";
+
+const toolBoxItems = [
+  {
+    title: "Javascript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: CssIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+];
+
+const hobbbies = [
+  {
+    title: "Painting",
+    emoji: "🎨",
+  },
+  {
+    title: "Photography",
+    emoji: "📸",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+  },
+  {
+    title: "Fitness",
+    emoji: "🏋️",
+  },
+  {
+    title: "Reading",
+    emoji: " 📚",
+  },
+];
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, who I am , and what inspires me."
+        />
+        <div className="mt-20">
+          <Card className="h-[320px]">
+            <CardHeader
+              title="My Reads"
+              description="Explore the books shaping my perspectives"
+            />
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="book cover" />
+            </div>
+          </Card>
+          <Card className="h-[320px] p-0">
+            <CardHeader
+              title="My Toolbox"
+              description="Explore the technologies and tools I use to create digital
+                experiences"
+              className="px-6 pt-6"
+            />
+            <ToolBoxItem items={toolBoxItems} className="mt-6" />
+            <ToolBoxItem items={toolBoxItems} className="mt-6" />
+          </Card>
+          <Card>
+            <CardHeader
+              title="Beyond the Code"
+              description="Explore my interest and hobbbies beyond the digital realm"
+            />
+            <div>
+              {hobbbies.map((hobby) => (
+                <div key={hobby.title}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="memoji" />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
